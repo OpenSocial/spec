6929d957be54e04d6cfab889705e0ad085a995e1
doll.cassie@gmail.com <doll.cassie@gmail.com@ad43269c-ce3d-0410-a9fc-fd0eb8aef9a5>
2008-08-12 17:26:01 +0000

    Added opensocial.IdSpec.GroupId enum as approved by the spec list.
    git-svn-id: http://opensocial-resources.googlecode.com/svn/spec@782 ad43269c-ce3d-0410-a9fc-fd0eb8aef9a5

--- 0.8/opensocial/idspec.js
+++ 0.8/opensocial/idspec.js
@@ -60,13 +60,15 @@ opensocial.IdSpec = function() {};
  */
 opensocial.IdSpec.Field = {
   /**
-   * A string or an array of strings representing the user id.
+   * A string or an array of strings representing the user id. Can be 
+   * one of the opensocial.IdSpec.PersonId values. 
    * @member opensocial.IdSpec.Field
    */
   USER_ID : 'userId',
 
   /**
-   * A string representing the group id.
+   * A string representing the group id or one of the
+   * opensocial.IdSpec.GroupId values. Defaults to SELF.
    * @member opensocial.IdSpec.Field
    */
   GROUP_ID : 'groupId',
@@ -105,6 +107,28 @@ opensocial.IdSpec.PersonId = {
   * @member opensocial.IdSpec.PersonId
   */
   VIEWER : 'VIEWER'
+};
+
+/**
+ * @static
+ * @class
+ * Constant group IDs available when fetching collections of people.
+ *
+ * @name opensocial.IdSpec.GroupId
+ */
+opensocial.IdSpec.GroupId = {
+ /**
+  * @member opensocial.IdSpec.GroupId
+  */
+  SELF : 'SELF',
+ /**
+  * @member opensocial.IdSpec.GroupId
+  */
+  FRIENDS : 'FRIENDS',
+ /**
+  * @member opensocial.IdSpec.GroupId
+  */
+  ALL : 'ALL'
 };
 
 /**
